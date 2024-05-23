bb1595a184556c821a19dab3785e297d40aec3ee
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2024-05-22 22:16:17 -0300

    Mais ajustes de frontend

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Paciente} from "../../models/paciente";
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+import {PacienteService} from "../../services/paciente.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-header',
@@ -7,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(
-  ) { }
+  usuarioLogado: Paciente
+  nmUsuario: string = ''
+
+  constructor(private router: Router,
+              private authService: AuthService,
+              private clienteService: PacienteService,
+              private toast: ToastrService) { }
 
   ngOnInit(): void {
+    this.router.navigate(['home'])
+    this.findAdmin();
+  }
+
+  findAdmin(): void {
+    let id = localStorage.getItem('id');
+    this.clienteService.findById(id).subscribe(resposta => {
+      this.usuarioLogado = resposta;
+      this.nmUsuario = resposta.nome;
+    })
+  }
+
+  logout() {
+    this.router.navigate(['login'])
+    this.authService.logout();
+    this.toast.info('Logout Realizado', 'Logout', {timeOut: 3000})
   }
 
 }
